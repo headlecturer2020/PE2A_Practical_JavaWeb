f01bc57bf142ae02f6cb9ce041188ffc3dd88b5d
students/Practical_JavaWeb_SE1200_20200429/SE62847 <students/Practical_JavaWeb_SE1200_20200429/SE62847>
2020-04-28 15:04:08 +0700

    students/Practical_JavaWeb_SE1200_20200429/SE62847

---
+++ Server/src/main/java/com/practicalexam/student/TemplateQuestion.java
@@ -0,0 +1,28 @@
+package com.practicalexam.student;
+
+
+
+import java.io.Serializable;
+
+public class TemplateQuestion implements Serializable {
+
+    public static boolean checkLogin(String username, String password) {
+        boolean check = false;
+       
+        return check;
+    }
+
+    public static boolean delete(String armorId) {
+        boolean check = false;
+       
+        return check;
+    }
+
+    public static int showAll() {
+        int result = -1;
+       
+
+        return result;
+    }
+
+}
