63c04c66d7da7b6f3774f3dd141377c17d86f5c7
students/Practical_JavaWeb_SE1200_20200427/SE63146 <students/Practical_JavaWeb_SE1200_20200427/SE63146>
2020-05-08 22:59:11 +0700

    students/Practical_JavaWeb_SE1200_20200427/SE63146

--- Server/src/test/java/server/TestResultLoggerExtension.java
+++ Server/src/test/java/server/TestResultLoggerExtension.java
@@ -113,7 +113,7 @@ public class TestResultLoggerExtension implements TestWatcher, AfterAllCallback 
         try {
             studentPointDto = setResult(studentPointDto);
         } catch (Exception e) {
-            System.out.println(e.printStackTrace());
+            e.printStackTrace();
             if (studentPointDto == null) {
                 studentPointDto = new StudentPointDto();
             }
