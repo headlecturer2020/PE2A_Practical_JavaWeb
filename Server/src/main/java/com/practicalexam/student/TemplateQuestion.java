fb1e843674a1df12dc88d50c75e7d8a1ca89d2bf
students/Practical_JavaWeb_SE1200_20200514/SE62882 <students/Practical_JavaWeb_SE1200_20200514/SE62882>
2020-05-13 22:02:47 +0700

    students/Practical_JavaWeb_SE1200_20200514/SE62882

--- Server/src/main/java/com/practicalexam/student/TemplateQuestion.java
+++ Server/src/main/java/com/practicalexam/student/TemplateQuestion.java
@@ -6,9 +6,9 @@ import java.io.Serializable;
 
 public class TemplateQuestion implements Serializable {
 
+
     private static TblUserDAO userDao = new TblUserDAO();
     private static TblWeaponDAO weaponDao = new TblWeaponDAO();
-
 
     public static boolean checkLogin(String username, String password) {
         boolean check = false;
